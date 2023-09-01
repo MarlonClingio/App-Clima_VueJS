6b70f291fe8210b9be9f3a1d59f51e3ac1676ae3
unknown <Marlon Clingio@DESKTOP-NBJ0U4R.(none)>
2023-09-01 00:35:32 -0300

    App Clima

---
+++ src/PaginaPrincipal.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <Cabecalho>
+
+    <template v-slot:subtitulo>
+      <h2>Previsão Meteorológica</h2>
+    </template>
+
+    <template v-slot:extra>
+      <h3>Saiba qual é a previsão do tempo para os próximos dias em sua Cidade!</h3>
+    </template>
+
+  </Cabecalho>
+
+  <ListaCidades />
+</template>
+
+
+<script>
+import "./assets/estilo.css";
+import Cabecalho from "./components/Cabecalho.vue";
+import ListaCidades from "./components/ListaCidades.vue";
+
+export default {
+  components: { Cabecalho, ListaCidades },
+
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style></style>
---
+++ src/components/ListaCidades.vue
@@ -0,0 +1,49 @@
+<template>
+  <h2>{{ cidadeselecionada }}</h2>
+  <br>
+  <div class="lista">
+    <CardCidade v-for="cidade in cidades" :key="cidade.id" :cidade="cidade" v-on:cidadeclicada="mudarcidadeselecionada" />
+  </div>
+</template>
+
+<script>
+import CardCidade from "./CardCidade.vue";
+export default {
+  components: { CardCidade },
+  methods: {
+    mudarcidadeselecionada(cidade) {
+      alert(cidade.name)
+    }
+  },
+  data() {
+    return {
+      cidadeselecionada: "",
+      cidades: [
+        {
+          id: 8429,
+          name: "Rio de Janeiro",
+          description: "Nublado",
+          icon: "01d",
+          temp: "25",
+        },
+        {
+          id: 8412,
+          name: "Manaus",
+          description: "Chuvas Fortes",
+          icon: "09d",
+          temp: "21",
+        },
+        {
+          id: 8454,
+          name: "Brasília",
+          description: "Ensolarado",
+          icon: "e81a",
+          temp: "27.4",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
